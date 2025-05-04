48efc457457dbd4c2a075eca87c01b9d9748d91e
harijagan <jaganhari2002@gmail.com>
2025-05-04 12:57:18 +0530

    protected the resume route

--- app/routes/resume_route.py
+++ app/routes/resume_route.py
@@ -1,12 +1,13 @@
 import json
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
 from app.utils.resume_reader import extract_resume_text
 from app.gpt_model.resume_parser import extract_resume_data
-
+from app.authentication.auth import get_current_user
+from app.model.user_auth import User
 router = APIRouter()
 
 @router.post("/upload-resume")
-async def upload_resume(file: UploadFile = File(...)):
+async def upload_resume(file: UploadFile = File(...),current_user:User = Depends(get_current_user)):
     if not file.filename.endswith((".pdf", ".docx")):
         raise HTTPException(status_code=400, detail="Only PDF or DOCX files are allowed")
 
@@ -17,7 +18,7 @@ async def upload_resume(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Failed to extract resume text")
 
     gpt_response = extract_resume_data(text)
-    print("Raw GPT Response:", gpt_response)
+    # print("Raw GPT Response:", gpt_response)
 
     gpt_text = gpt_response.get("response", "")
 
